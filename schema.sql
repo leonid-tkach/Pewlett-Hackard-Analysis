2b4983836f41d03bd88085127e93468eada75254
leonid-tkach <79228337+leonid-tkach@users.noreply.github.com>
2022-05-31 16:38:11 -0400

    queries

--- schema.sql
+++ schema.sql
@@ -44,13 +44,15 @@ CREATE TABLE dept_emp (
     PRIMARY KEY (emp_no, dept_no)
 );
 
+DROP TABLE titles CASCADE;
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+--	PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_manager;
